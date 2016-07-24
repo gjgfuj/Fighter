a3e42e2510475f5793d56bdefa3522b89758afff
Sandra <gjgfujb@gmail.com>
2016-07-24 17:30:16 +1000

    Rect Collisions work.

--- rect.lua
+++ rect.lua
@@ -4,8 +4,11 @@ function rect:__index(key)
 	return rawget(rect,key)
 end
 function rect:collide(other)
-	if (other.x >= self.x and other.x <= self.endx) or (other.endx >= self.x and other.endx <= self.endx) then
-		if (other.y >= self.y and other.y <= self.endy) or (other.endy >= self.y and other.endy <= self.endy) then
+	return self:__basecollide(other) or other:__basecollide(self)
+end
+function rect:__basecollide(other)
+	if (other.x >= self.x and other.x <= self.endx) or (other.endx >= self.x and other.endx <= self.endx) or (other.x <= self.x and other.endx >= self.x) then
+		if (other.y >= self.y and other.y <= self.endy) or (other.endy >= self.y and other.endy <= self.endy) or (other.y <= self.y and other.endy >= self.y) then
 			return true
 		end
 	end
